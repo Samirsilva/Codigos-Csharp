eb789c469708eb1d1b8265c29a4178505d450700
Samir Silva <samirlass@hotmail.com>
2017-02-20 09:50:08 -0300

    Update Funcoes_de_colisao_e_trigger.cs

--- Unity Games 2D/Funcoes_de_colisao_e_trigger.cs
+++ Unity Games 2D/Funcoes_de_colisao_e_trigger.cs
@@ -1,30 +1,29 @@
-        void OnCollisionEnter2D(Collision2D nomeParametro){// entra em colisao com um colisor
+void OnCollisionEnter2D(Collision2D nomeParametro){// entra em colisao com um colisor
+
+        print ("Entru em colisao com um colidor");
+}
  
-                print ("Entru em colisao com um colidor");
+void OnTriggerEnter2D(Collider2D nomeParametro){//Entra em colisao com um trigger
  
-        }
+        print ("Entra em colisao com um trigger");
+}
  
-        void OnTriggerEnter2D(Collider2D nomeParametro){//Entra em colisao com um trigger
+void OnCollisionExit2D(){//Detecta a saida da colisao
  
-                print ("Entra em colisao com um trigger");
-        }
+        print ("Detectou a saida da colisao");
+}
  
-        void OnCollisionExit2D(){//Detecta a saida da colisao
+void OnTriggerExit2D(){//Detecta a saida da colisao com um trigger
  
-                print ("Detectou a saida da colisao");
-        }
+        print ("Detecta a saida da colisao com um trigger");
+}
  
-        void OnTriggerExit2D(){//Detecta a saida da colisao com um trigger
+void OnCollisionStay2D(){//Detecta se eu anda estou colidindo.
  
-                print ("Detecta a saida da colisao com um trigger");
-        }
+        print("Ainda esta em colisao com um colisor");
+}
  
-        void OnCollisionStay2D(){//Detecta se eu anda estou colidindo.
- 
-                print("Ainda esta em colisao com um colisor");
-        }
- 
-        void OnTriggerStay2D(){//Detecta se eu anda estou colidindo com um trigger.
+void OnTriggerStay2D(){//Detecta se eu anda estou colidindo com um trigger.
                
-                print("Ainda esta em colisao com um trigger");
-        }
+        print("Ainda esta em colisao com um trigger");
+}
